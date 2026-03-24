20ab70c3102bfcdb7402c5fccb2c0a03b34cb8dd
sheepbacon2 <129823945+sheepbacon2@users.noreply.github.com>
2026-03-24 14:11:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,38 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
 import pandas
+import datetime as dt
 import random
 import smtplib
-import os
+my_email = os.environ.get("my_email")
+password = os.environ.get("password")
+recipient = ""
+def random_letter():
+    letters = ["letter_templates/letter_1.txt", "letter_templates/letter_2.txt", "letter_templates/letter_3.txt"]
+    return random.choice(letters)
+def get_letter(name):
+    letter = random_letter()
+    with open(letter, "r") as file:
+        content = file.read()
+        new_content = content.replace("[NAME]", name)
+        return new_content
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
-
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+now = dt.datetime.now()
+month = now.month
+day = now.day
+today_date = [month,day]
+df = pandas.read_csv("birthdays.csv")
+birthdays = df.to_dict("records")
+for birthday in birthdays:
+    if birthday["month"] == month and birthday["day"] == day:
+        letter = get_letter(birthday["name"])
+        recipient = birthday["email"]
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=recipient,
+                msg=f"Subject:Birthday Wish\n\n {letter}"
+            )
