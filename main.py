e661c64d5f14ba6045cdbf8450e1c4ae1fd52397
sheepbacon2 <129823945+sheepbacon2@users.noreply.github.com>
2026-04-01 20:42:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 from datetime import datetime
 import requests
 
-my_lat = secrets.MY_LAT
-my_lon = secrets.MY_LON
+my_lat = MY_LAT
+my_lon = MY_LON
 account_sid = os.environ.get("ACCOUNT_SID")
 auth_token = os.environ.get("AUTH_TOKEN")
 client = Client(account_sid, auth_token)
